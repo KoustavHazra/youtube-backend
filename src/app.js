536d800b7805aec8c7181dfb8747305ef135c34e
KoustavHazra <hazrakoustav12@gmail.com>
2024-04-12 02:29:36 +0530

    added like, comment, video, playlist, subscription, dashboard, healthcheck, tweet controller , routes || last commit of version 0.1 || next commit version will be 0.2

--- src/app.js
+++ src/app.js
@@ -32,6 +32,14 @@ app.use(cookieParser());
 
 // routes import
 import userRoute from "./routes/user.routes.js";
+import healthcheckRouter from "./routes/healthcheck.routes.js";
+import tweetRouter from "./routes/tweet.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import videoRouter from "./routes/video.routes.js";
+import commentRouter from "./routes/comment.routes.js";
+import likeRouter from "./routes/like.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
 
 // routes decleration
 app.use("/api/v1/users", userRoute);  // anytime user goes to /users, they will be redirected to userRoute router.
@@ -39,6 +47,14 @@ app.use("/api/v1/users", userRoute);  // anytime user goes to /users, they will 
 // but since router is in other directory, to bring it here we will be needing middleware, hence we are using .use()
 // /api/v1 are just to show that version 1 API is being used. If later we change to version 2, then it will be /api/v2 ...
 
+app.use("/api/v1/healthcheck", healthcheckRouter);
+app.use("/api/v1/tweets", tweetRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
 
 
 export { app };
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,8 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { apiError } from "../utils/apiError.js";
+import { apiResponse } from "../utils/apiResponse.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
-import { apiResponse } from "../utils/apiResponse.js";
 import jwt from "jsonwebtoken";
 import { mongoose } from "mongoose";
 
