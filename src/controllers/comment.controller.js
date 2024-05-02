1f18f030e3579f64673248ece3024825cd410de0
KoustavHazra <hazrakoustav12@gmail.com>
2024-05-02 19:09:28 +0530

    fixed the comments-controller issue || and with this, this project is completed.

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -9,21 +9,38 @@ import { User } from "../models/user.model.js";
 const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
     const { videoId } = req.params;
-    const { page = 1, limit = 10 } = req.query;
+    const { page = 1, limit = 10 } = req.body;
 
     // video exists ?
-    // const video = await Video.findById(videoId);
-    // if (!video) throw new apiError(400, "VIDEO NOT FOUND.");
+    const video = await Video.findById(videoId);
+    if (!video) throw new apiError(400, "VIDEO NOT FOUND.");
 
-    // // comments exists ?
-    // const comments = await Comment.paginate(
-    //     { video: videoId },
-    //     { page: parseInt(page), limit: parseInt(limit), sort: { createdAt: -1 } }
-    // );
-    // if (!comments) throw new apiError(400, "COMMENTS NOT FOUND.");
+    // comments exists ?
+    const comments = await Comment.aggregate([
+        {
+            $match: {
+                video: new mongoose.Types.ObjectId(videoId)
+            }
+        },
+        {
+            $skip: (page - 1) * limit,
+        },
+        {
+            $limit: limit
+        },
+        {
+            $project: {
+                comment: 1,
+                owner: 1,
+                _id: 0
+            }
+        }
+    ]);
 
-    // if (comments.length === 0) return res.status(200).json(new apiResponse(200, {}, "No comment found for this video."));
-    // else return res.status(200).json(new apiResponse(200, comments, "Comments fetched successfully."));
+    if (!comments) throw new apiError(400, "COMMENTS NOT FOUND.");
+
+    if (comments.length === 0) return res.status(200).json(new apiResponse(200, {}, "No comment found for this video."));
+    else return res.status(200).json(new apiResponse(200, comments, "Comments fetched successfully."));
 
 })
 
@@ -47,48 +64,46 @@ const addComment = asyncHandler(async (req, res) => {
 
 const updateComment = asyncHandler(async (req, res) => {
     // TODO: update a comment
-    const { videoId, commentId } = req.body;
+    const { videoId, commentId } = req.params;
+    const { editedComment } = req.body;
 
     // user is verified ?
-    // const user = await User.findById(req.isVerifiedUser._id).select(
-    //     " -password -refreshToken -avatar -coverImage -createdAt -updatedAt -watchHistory -email "
-    // );
-    // if (!user) throw new apiError(400, "USER IS NOT VERIFIED. PLEASE LOGIN.");
+    const user = await User.findById(req.isVerifiedUser._id);
+    if (!user) throw new apiError(400, "USER IS NOT VERIFIED. PLEASE LOGIN.");
 
-    // // video exists ?
-    // const video = await Video.findById(videoId);
-    // if (!video) throw new apiError(400, "VIDEO NOT FOUND.");
+    // video exists ?
+    const video = await Video.findById(videoId);
+    if (!video) throw new apiError(400, "VIDEO NOT FOUND.");
 
-    // // comment exists
-    // const comment = await Comment.findById(commentId);
-    // if (!comment) throw new apiError(400, "COMMENT NOT FOUND.");
+    // comment exists
+    const comment = await Comment.findById(commentId);
+    if (!comment) throw new apiError(400, "COMMENT NOT FOUND.");
 
-    // // update comment
-    // const updatedComment = await Comment.findByIdAndUpdate(
-    //     commentId,
-    //     {
-    //         $set: {
-    //             comment: comment
-    //         }
-    //     },
-    //     {
-    //         new: true
-    //     }
-    // );
-    // if (!updateComment) throw new apiError(400, "COMMENT NOT UPDATED.");
+    // update comment
+    const updatedComment = await Comment.findByIdAndUpdate(
+        commentId,
+        {
+            $set: {
+                comment: editedComment
+            }
+        },
+        {
+            new: true,
+            runValidators: true
+        }
+    );
+    if (!updatedComment) throw new apiError(400, "COMMENT NOT UPDATED.");
 
-    // return res.status(200).json(new apiResponse(200, updateComment, "Comment is updated."));
+    return res.status(200).json(new apiResponse(200, updatedComment, "Comment is updated."));
 
 })
 
 const deleteComment = asyncHandler(async (req, res) => {
     // TODO: delete a comment
-    const { videoId, commentId } = req.body;
+    const { videoId, commentId } = req.params;
 
     // user is verified ?
-    const user = await User.findById(req.isVerifiedUser._id).select(
-        " -password -refreshToken -avatar -coverImage -createdAt -updatedAt -watchHistory -email "
-    );
+    const user = await User.findById(req.isVerifiedUser._id);
     if (!user) throw new apiError(400, "USER IS NOT VERIFIED. PLEASE LOGIN.");
 
     // video exists ?
